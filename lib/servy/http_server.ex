ebdac7a582ec50e2ec5454923afad46a23769236
ryan <ryanyogan@gmail.com>
2021-07-05 15:36:12 -0500

    first pass at http/tcp server

---
+++ lib/servy/http_server.ex
@@ -0,0 +1,79 @@
+defmodule Servy.HttpServer do
+  @doc """
+  Starts the server on the given `port` of localhost.
+  """
+  def start(port) when is_integer(port) and port > 1023 do
+    {:ok, listen_socket} =
+      :gen_tcp.listen(
+        port,
+        [:binary, packet: :raw, active: false, reuseaddr: true]
+      )
+
+    IO.puts("Listening for connection on port #{port}...\n")
+
+    accept_loop(listen_socket)
+  end
+
+  @doc """
+  Accepts client connections on the `listen_socket`.
+  """
+  def accept_loop(listen_socket) do
+    IO.puts("Waiting to accept a client connection...\n")
+
+    {:ok, client_socket} = :gen_tcp.accept(listen_socket)
+
+    IO.puts("Connection accepted!\n")
+
+    serve(client_socket)
+
+    accept_loop(listen_socket)
+  end
+
+  @doc """
+  Receives the request on the `client_socket` and
+  sends a response back over the same socket.
+  """
+  def serve(client_socket) do
+    client_socket
+    |> read_request()
+    |> Servy.Handler.handle()
+    |> write_response(client_socket)
+  end
+
+  @doc """
+  Receives a request on the `client_socket`.
+  """
+  def read_request(client_socket) do
+    {:ok, request} = :gen_tcp.recv(client_socket, 0)
+
+    IO.puts("Received request:\n")
+    IO.puts(request)
+
+    request
+  end
+
+  @doc """
+  Returns a generic HTTP response.
+  """
+  def generate_response(_request) do
+    """
+    HTTP/1.1 200 OK\r
+    Content-Type: text/plain\r
+    Content-Length: 6\r
+    \r
+    Hello!
+    """
+  end
+
+  @doc """
+  Sends the `response` over the `client_socket`.
+  """
+  def write_response(response, client_socket) do
+    :ok = :gen_tcp.send(client_socket, response)
+
+    IO.puts("Sent response:\n")
+    IO.puts(response)
+
+    :gen_tcp.close(client_socket)
+  end
+end
