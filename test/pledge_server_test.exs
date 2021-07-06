183ce2c1193d3eeb43126cd2abdd52392e631346
ryan <ryanyogan@gmail.com>
2021-07-06 10:03:31 -0500

    pledge server in green

---
+++ test/pledge_server_test.exs
@@ -0,0 +1,21 @@
+defmodule PledgeServerTest do
+  use ExUnit.Case
+
+  alias Servy.PledgeServer
+
+  test "caches the 3 most recent pledges and totals their amount" do
+    PledgeServer.start()
+
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 10)
+    PledgeServer.create_pledge("curly", 20)
+    PledgeServer.create_pledge("daisy", 30)
+    PledgeServer.create_pledge("grace", 40)
+    PledgeServer.create_pledge("larry", 10)
+
+    most_recent_pledges = [{"larry", 10}, {"grace", 40}, {"daisy", 30}]
+
+    assert PledgeServer.recent_pledges() == most_recent_pledges
+    assert PledgeServer.total_pledges() == 80
+  end
+end
